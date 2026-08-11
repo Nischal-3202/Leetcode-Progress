ec809ace05d82e8bbdb0ce418f059dbd7ca9cc92
Nischal Poudel <nischalpoudel2004@gmail.com>
2026-08-11 22:55:49 +0530

    Time: 13 ms (34.4%), Space: 14.4 MB (10.75%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maj_ele=nums[0]
+        count=1
+        for i in range(1,len(nums)):
+            if count == 0:
+                maj_ele=nums[i]
+            if nums[i]==maj_ele :
+                count+=1
+            else:
+                count -=1
+
+        return maj_ele
