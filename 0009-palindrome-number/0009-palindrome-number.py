f09344f72877810013a328c3236ab48662b32c92
Nischal Poudel <nischalpoudel2004@gmail.com>
2026-08-21 20:33:08 +0530

    Time: 4 ms (90.72%), Space: 12.3 MB (90.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,6 +4,4 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if str(x)==str(x)[::-1]:
-            return True
-        return False
+        return str(x)==str(x)[::-1]
