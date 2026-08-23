0a54977ffb7a531bfa11c320825e2554c55de7d0
Nischal Poudel <nischalpoudel2004@gmail.com>
2026-08-23 21:19:00 +0530

    Time: 0 ms (100%), Space: 12.6 MB (87.98%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res=[]
+        open_count=0
+        close_count=0
+        current=""
+        def backtrack(current,open_count,close_count):
+            if len(current) == 2 * n:
+                res.append(current)
+                return
+            if open_count < n:
+                backtrack(current+"(",open_count+1,close_count)
+            if close_count < open_count:
+                backtrack(current+")",open_count,close_count+1)
+        backtrack("",0,0)
+        return res
+            
