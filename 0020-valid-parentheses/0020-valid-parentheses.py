cf7bf8abd0431478dee36c875e18f0b7b7ad84bf
Nischal Poudel <nischalpoudel2004@gmail.com>
2026-08-23 19:23:58 +0530

    Time: 4 ms (41.42%), Space: 12.6 MB (39.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        res=[]
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                res.append(char)
+            elif char == ')' and res and res[-1]=='(':
+                res.pop()
+            elif char == '}' and res and res[-1]=='{':
+                res.pop()
+            elif char == ']' and res and res[-1]=='[':
+                res.pop()
+            else:
+                return False
+        return len(res)==0
