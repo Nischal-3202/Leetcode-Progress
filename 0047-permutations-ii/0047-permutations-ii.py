0a41ec99751490a6e9a3329fe72253304376a41a
Nischal Poudel <nischalpoudel2004@gmail.com>
2026-08-31 23:27:40 +0530

    Time: 9 ms (48.36%), Space: 12.9 MB (78.15%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res=[]
+        path=[]
+        used=[False]*len(nums)
+        def backtrack(path,used):
+            if len(path)==len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i>0 and nums[i]==nums[i-1] and not used[i-1]:
+                    continue
+                used[i]=True
+                path.append(nums[i])
+                backtrack(path,used)
+                path.pop()
+                used[i]=False
+        backtrack(path,used)
+        return res
